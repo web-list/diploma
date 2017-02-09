3cb3eefc02538ebb8788c838d6d5bdd965c20daf
Programmist <717720@list.ru>
2017-02-09 19:02:12 +0900

    commit

--- protected/models/Period.php
+++ protected/models/Period.php
@@ -7,7 +7,14 @@ class Period
 
   public function plusHalfMonth() {
 
-   return $this->time;
+    $day = date("j", $this->time);
+
+    if ($day > 15) {
+      $this->time = strtotime("+1 month", $this->time);
+      $this->time = strtotime("-15 days", $this->time);
+    } else {
+      $this->time = strtotime("+15 days", $this->time);
+    }
 
   }
 
